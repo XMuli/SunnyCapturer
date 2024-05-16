b92cbe5c5aac3e1783fd7ad8cf1be33ab22418ee
XMuli <xmulitech@gmail.com>
2024-05-16 22:55:58 +0800

    fix: linux using postinst postrm script fails
    1. It could be the cpack set order.
    2. postinst postrm before deb package requires permission chmod +x postinst
    
    on ubuntu22.04

--- src/main.cpp
+++ src/main.cpp
@@ -129,7 +129,7 @@ int main(int argc, char* argv[])
     }
 
 
-    CJ.initPrepare();
+//    CJ.initPrepare();
     CJ.readFromFile();
     CJ.initOutputDefaulValue(); // 完整的 m_j = 读取 .josn + 填充为空 path 的一些路径
     CJ.writeToFile(); // 写入完整的 m_j
